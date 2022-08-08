a68bc15c25d75c6fb3cda09f30ada33406e8e05d
gitradonz <63265564+gitradonz@users.noreply.github.com>
2022-08-08 22:37:56 +0200

    Installed tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from "./logo.svg";
-import "./App.css";
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hector López</p>
+        <p className="text-sm">Héctor López</p>
       </header>
     </div>
   );
