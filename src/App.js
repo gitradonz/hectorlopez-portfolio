9027c7822c5f73299d3082a0f3dc46669c7baac8
gitradonz <63265564+gitradonz@users.noreply.github.com>
2022-08-08 22:46:57 +0200

    WIP

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p className="text-sm">Héctor López</p>
+    <div>
+      <header className="h-32 bg-yellow-100 p-3">
+        <p className="text-3xl font-extrabold text-center">Héctor López</p>
+        <p className="text-sm font-semibold text-center mt-3">
+          Work in progress
+        </p>
       </header>
     </div>
   );
