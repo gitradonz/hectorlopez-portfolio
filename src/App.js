dee1e7f2f86ee7fde3de72e52fc9197e9f1559cd
gitradonz <63265564+gitradonz@users.noreply.github.com>
2022-08-09 23:18:21 +0200

    Added react-router-dom + some routes and main layout

--- src/App.js
+++ src/App.js
@@ -1,20 +1,50 @@
 import logo from "./logo.svg";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <header>
-        <nav className="h-32 flex justify-evenly items-center ">
-          <img src={logo} className="w-10 h-10" alt="logo" />
-          <p className="text-3xl font-bold text-center">About</p>
-          <p className="text-3xl font-bold text-center">Works</p>
-          <p className="text-3xl font-bold text-center">5505</p>
-          <p className="text-3xl font-bold text-center">Contact</p>
-        </nav>
-      </header>
-      <body className="h-screen bg-neutral-800"></body>
-    </div>
+    <Router>
+      <nav className="absolute h-screen w-screen flex justify-evenly p-7 bg-transparent">
+        <Link to="/about" className="text-3xl font-bold text-center">
+          About
+        </Link>
+        <Link to="/work" className="text-3xl font-bold text-center">
+          Works
+        </Link>
+        <Link to="/5505" className="text-3xl font-bold text-center">
+          5505
+        </Link>
+        <Link to="/contact" className="text-3xl font-bold text-center">
+          Contact
+        </Link>
+        <img className="absolute top-1/2 left-5 w-20" src={logo} alt="logo" />
+        <div className="absolute top-1/2 right-10 ">{"hectxrlxpez®"}</div>
+      </nav>
+
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/5505" element={<FiveFiveOFive />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
+function FiveFiveOFive() {
+  return <h2>5505</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Work() {
+  return <h2>Work</h2>;
+}
+
+function Contact() {
+  return <h2>Contact</h2>;
+}
