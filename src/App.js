37df2a03014e1ff8fe95ce6fc266d523eaa84a61
gitradonz <63265564+gitradonz@users.noreply.github.com>
2022-08-09 23:28:57 +0200

    Added pages to transitions

--- src/App.js
+++ src/App.js
@@ -4,24 +4,27 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <Router>
-      <nav className="absolute h-screen w-screen flex justify-evenly p-7 bg-transparent">
+      <div className="absolute h-screen w-screen flex justify-evenly p-7 bg-transparent">
         <Link to="/about" className="text-3xl font-bold text-center">
           About
         </Link>
-        <Link to="/work" className="text-3xl font-bold text-center">
+        <Link to="/work" className="text-3xl font-bold text-center h-10">
           Works
         </Link>
-        <Link to="/5505" className="text-3xl font-bold text-center">
+        <Link to="/5505" className="text-3xl font-bold text-center h-10">
           5505
         </Link>
-        <Link to="/contact" className="text-3xl font-bold text-center">
+        <Link to="/contact" className="text-3xl font-bold text-center h-10">
           Contact
         </Link>
-        <img className="absolute top-1/2 left-5 w-20" src={logo} alt="logo" />
+        <Link to="/" className="text-3xl font-bold text-center h-10">
+          <img className="absolute top-1/2 left-5 w-20" src={logo} alt="logo" />
+        </Link>
         <div className="absolute top-1/2 right-10 ">{"hectxrlxpez®"}</div>
-      </nav>
+      </div>
 
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/work" element={<Work />} />
         <Route path="/5505" element={<FiveFiveOFive />} />
@@ -34,17 +37,23 @@ function App() {
 export default App;
 
 function FiveFiveOFive() {
-  return <h2>5505</h2>;
+  return <div className="h-screen flex justify-center items-center">5505</div>;
 }
 
 function About() {
-  return <h2>About</h2>;
+  return <div className="h-screen flex justify-center items-center">ABOUT</div>;
 }
 
 function Work() {
-  return <h2>Work</h2>;
+  return <div className="h-screen flex justify-center items-center">WORK</div>;
 }
 
 function Contact() {
-  return <h2>Contact</h2>;
+  return (
+    <div className="h-screen flex justify-center items-center">CONTACT</div>
+  );
+}
+
+function Home() {
+  return <div className="h-screen bg-gray-800" />;
 }
