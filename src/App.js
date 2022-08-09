159253158aab9b7d781fdfe01c3283e4391b5d8f
gitradonz <63265564+gitradonz@users.noreply.github.com>
2022-08-09 23:36:15 +0200

    Placeholder image + fixed navbar size

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import logo from "./logo.svg";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import placeholder from "../src/Images/placeholder.jpg";
 
 function App() {
   return (
     <Router>
       <div className="absolute h-screen w-screen flex justify-evenly p-7 bg-transparent">
-        <Link to="/about" className="text-3xl font-bold text-center">
+        <Link
+          to="/about"
+          className="text-3xl font-bold text-center text-gray-300"
+        >
           About
         </Link>
-        <Link to="/work" className="text-3xl font-bold text-center h-10">
+        <Link
+          to="/work"
+          className="text-3xl font-bold text-center h-10 text-gray-300"
+        >
           Works
         </Link>
-        <Link to="/5505" className="text-3xl font-bold text-center h-10">
+        <Link
+          to="/5505"
+          className="text-3xl font-bold text-center h-10 text-gray-300"
+        >
           5505
         </Link>
-        <Link to="/contact" className="text-3xl font-bold text-center h-10">
+        <Link
+          to="/contact"
+          className="text-3xl font-bold text-center h-10 text-gray-300"
+        >
           Contact
         </Link>
-        <Link to="/" className="text-3xl font-bold text-center h-10">
-          <img className="absolute top-1/2 left-5 w-20" src={logo} alt="logo" />
-        </Link>
-        <div className="absolute top-1/2 right-10 ">{"hectxrlxpez®"}</div>
       </div>
-
+      <Link to="/" className="text-3xl font-bold text-center h-10">
+        <img className="absolute top-1/2 left-5 w-20" src={logo} alt="logo" />
+      </Link>
+      <div className="absolute top-1/2 right-10 text-gray-300">
+        {"hectxrlxpez®"}
+      </div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -55,5 +69,7 @@ function Contact() {
 }
 
 function Home() {
-  return <div className="h-screen bg-gray-800" />;
+  return (
+    <img className="h-screen w-screen" src={placeholder} alt="placeholder" />
+  );
 }
