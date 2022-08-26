d07fb561c4d7b67757ddb9ab0eaabf0aa84dd12b
gitradonz <63265564+gitradonz@users.noreply.github.com>
2022-08-26 23:28:19 +0200

    Added about layout

--- src/sections/About.js
+++ src/sections/About.js
@@ -3,12 +3,14 @@ import { motion } from "framer-motion";
 export default function About() {
   return (
     <motion.div
-      className="h-screen flex justify-center items-center bg-gray-800"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1, transition: { duration: 1 } }}
       exit={{ opacity: 0 }}
     >
-      ABOUT
+      <div className="flex">
+        <div className="h-screen w-1/2 bg-gray"></div>
+        <div className="h-screen w-1/2 bg-black"></div>
+      </div>
     </motion.div>
   );
 }
